0727985e0c9cb837512e53a2cc8eb1835d47e826
xkendx <63884610+xkendx@users.noreply.github.com>
2021-01-20 16:37:39 +0300

    feat: add blog_api

--- src/bilgidenizi/urls.py
+++ src/bilgidenizi/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
     path('user/', include("user.urls")),
+    path('api/', include("blog_api.urls")),
 ]
+ 
