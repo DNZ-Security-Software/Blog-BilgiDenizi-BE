2cef984585522e90b0e87b4e9a484881fecc919c
xkendx <63884610+xkendx@users.noreply.github.com>
2021-01-22 23:34:00 +0300

    feat: create forms

--- src/blog_api/apps.py
+++ src/blog_api/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class BlogApiConfig(AppConfig):
     name = 'blog_api'
+
+    def ready(self):
+        import blog_api.signals
--- src/blog_api/models.py
+++ src/blog_api/models.py
@@ -1,34 +1,72 @@
-from typing import Optional
 from django.db import models
-from django.db.models.fields import BLANK_CHOICE_DASH
-from django.contrib.auth.models import User  # import django user model
-# Create your models here.
+from django.contrib.auth.models import User
+
 
 def user_directory_path(instance, filename):
     return 'blog/{0}/{1}'.format(instance.author.id, filename)
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
-    class Meta:
-        verbose_name_plural = "Categories"  # ismi çoğul yaptığında yanlış olanları düzeltme için
+
 
 class Post(models.Model):
     OPTIONS = (
         ('d', 'Draft'),
-        ('p', 'Publised')
+        ('p', 'Published')
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to= user_directory_path, default='dnz-security.png' )
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)  # Categoriye ait olan postların kategori silinince silinmesini engellemek için PROTECT
+    image = models.ImageField(
+        upload_to=user_directory_path, default='django.jpg')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # user silinince POST larında silinmesi için CASCADE
-    status = models.CharField(max_length=10, choices=OPTIONS, default="d") 
-    slug = models.SlugField(blank=True, unique=True)
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+    slug = models.SlugField(blank=True, unique=True)  # how-to-learn-django
+
     def __str__(self):
         return self.title
+
+    def comment_count(self):
+        return self.comment_set.all().count()
+
+    def view_count(self):
+        return self.postview_set.all().count()
+
+    def like_count(self):
+        return self.like_set.all().count()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class PostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
